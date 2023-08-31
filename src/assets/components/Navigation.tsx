357108c669e937b5f60501d9849eadd2338eba08
Pande <popovski.pande@gmail.com>
2023-08-31 23:53:02 +0200

    make navigation icons smaller

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -30,7 +30,7 @@ function Navigation() {
         {/* Nav Logo */}
         <a href='' aria-label="back to home" className="nav-logo">
           <img src="src\assets\icons\glasses-svgrepo-com.svg" aria-hidden="true" decoding="async"
-          className="w-10 h-auto" />
+          className="w-8 h-auto" />
         </a>
 
         {/* Navigation List & Toggle Dark Mode Wrapper */}
@@ -42,19 +42,19 @@ function Navigation() {
             {/* Mobile Nav Toggle */}
             <button aria-label="mobile menu toggle" aria-controls="nav-menu" 
             aria-expanded='false' onClick={toggleMenu}
-            className="w-10 h-10">
+            className="w-8 h-8">
               <div aria-hidden="true"
-              className="flex flex-col w-full h-full justify-around">
+              className="flex flex-col w-full h-full justify-center items-center gap-1">
                 <span aria-hidden="true" 
-                className="w-full h-1 bg-gray-800 rounded-md"
+                className="w-3/4 h-1 bg-gray-800 rounded-md"
                 ></span>
 
                 <span aria-hidden="true" 
-                className="w-full h-1 bg-gray-800 rounded-md"
+                className="w-3/4 h-1 bg-gray-800 rounded-md"
                 ></span>
 
                 <span aria-hidden="true" 
-                className="w-full h-1 bg-gray-800 rounded-md"
+                className="w-3/4 h-1 bg-gray-800 rounded-md"
                 ></span>
               </div>
             </button>
@@ -62,7 +62,7 @@ function Navigation() {
             {/* Nav Links */}
             {menuVisible && 
             <div
-            className='absolute top-10 right-0 p-4'>
+            className='absolute top-8 right-0 p-4'>
               <ul>
                 <li>
                   <a href="">Home</a>
@@ -87,12 +87,12 @@ function Navigation() {
           <button aria-label="dark mode toggle" onClick={toggleDarkMode}>
             {darkMode &&
             <img src="src\assets\icons\moon-svgrepo-com.svg" alt="moon" aria-hidden='true'
-            className="w-10 h-auto" />
+            className="w-8 h-auto" />
             }
 
             {!darkMode &&
             <img src="src\assets\icons\sun-svgrepo-com.svg" alt="sun" aria-hidden='true'
-            className="w-10 h-auto" />
+            className="w-8 h-auto" />
             }
           </button>
         </div>
