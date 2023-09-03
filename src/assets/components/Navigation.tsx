fcc46aedee00eda95f59fd6f39a78f858b7a57c4
Pande <popovski.pande@gmail.com>
2023-09-03 21:06:53 +0200

    add styles to mobile navigation

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -22,15 +22,34 @@ function Navigation() {
       document.removeEventListener('mousedown', handler);
     })
   });
+
+  // Animate toggle dark mode button when clicked
+  const animateToggleButton = () => {
+    const toggleModeButton = document.querySelector('.toggle-mode-button');
+
+    // Make button invisible when clicked
+    toggleModeButton?.classList.add('opacity-0');
+    // At the same moment make it visible and add transition so it fades in
+    setTimeout(() => {
+      toggleModeButton?.classList.add('transition-all');
+      toggleModeButton?.classList.add('duration-500');
+      toggleModeButton?.classList.remove('opacity-0');
+    }, 0);
+    // Remove transition effects after
+    setTimeout(() => {
+      toggleModeButton?.classList.remove('transition-all');
+      toggleModeButton?.classList.remove('duration-500');
+    }, 500);
+  }
   
   return(
-    <header className="navigation">
+    <header className="sticky top-0 left-0 bg-gray-900 z-20">
       <div 
-      className="navigation-container flex justify-between items-center px-4 py-2">
+      className="navigation-container flex justify-between items-center px-4 py-4 shadow-lg shadow-gray-800">
         {/* Nav Logo */}
         <a href='' aria-label="back to home" className="nav-logo">
           <img src="src\assets\icons\glasses-svgrepo-com.svg" aria-hidden="true" decoding="async"
-          className="w-8 h-auto" />
+          className="w-8 h-auto invert" />
         </a>
 
         {/* Navigation List & Toggle Dark Mode Wrapper */}
@@ -46,53 +65,56 @@ function Navigation() {
               <div aria-hidden="true"
               className="flex flex-col w-full h-full justify-center items-center gap-1">
                 <span aria-hidden="true" 
-                className="w-3/4 h-1 bg-gray-800 rounded-md"
+                className={"w-3/4 h-1 bg-white rounded-md transition-all duration-300 " + (menuVisible ? "rotate-45 translate-y-2" : "")}
                 ></span>
 
                 <span aria-hidden="true" 
-                className="w-3/4 h-1 bg-gray-800 rounded-md"
+                className={"w-3/4 h-1 bg-white rounded-md transition-all duration-300 " + (menuVisible ? "opacity-0": "")}
                 ></span>
 
                 <span aria-hidden="true" 
-                className="w-3/4 h-1 bg-gray-800 rounded-md"
+                className={"w-3/4 h-1 bg-white rounded-md transition-all duration-300 " + (menuVisible ? "-rotate-45 -translate-y-2" : "")}
                 ></span>
               </div>
             </button>
 
             {/* Nav Links */}
-            {menuVisible && 
             <div
-            className='absolute top-8 right-0 p-4'>
-              <ul>
+            className={'absolute w-screen top-12 -right-4  px-10 bg-gray-900 z-20 overflow-hidden transition-all duration-500 ease-in-out shadow-lg shadow-gray-800 ' + (!menuVisible ? "h-0" : "h-52")}>
+              <ul className={"py-2 flex flex-col gap-2 items-center"}>
                 <li>
-                  <a href="">Home</a>
+                  <a href="" className='header-nav-link'>Home</a>
                 </li>
                 <li>
-                  <a href="">About</a>
+                  <a href="" className='header-nav-link'>About</a>
                 </li>
                 <li>
-                  <a href="">Services</a>
+                  <a href="" className='header-nav-link'>Services</a>
                 </li>
                 <li>
-                  <a href="">Reviews</a>
+                  <a href="" className='header-nav-link'>Reviews</a>
                 </li>
                 <li>
-                  <a href="">Contact</a>
+                  <a href="" className='header-nav-link'>Contact</a>
                 </li>
               </ul>
-            </div>}
+            </div>
           </nav>
 
           {/* Dark Mode Toggle */}
-          <button aria-label="dark mode toggle" onClick={toggleDarkMode}>
+          <button aria-label="dark mode toggle" onClick={() => {
+            toggleDarkMode();
+            animateToggleButton();
+          }} 
+          className='toggle-mode-button'>
             {darkMode &&
             <img src="src\assets\icons\moon-svgrepo-com.svg" alt="moon" aria-hidden='true'
-            className="w-8 h-auto" />
+            className="w-8 h-auto invert" />
             }
 
             {!darkMode &&
             <img src="src\assets\icons\sun-svgrepo-com.svg" alt="sun" aria-hidden='true'
-            className="w-8 h-auto" />
+            className="w-8 h-auto invert" />
             }
           </button>
         </div>
