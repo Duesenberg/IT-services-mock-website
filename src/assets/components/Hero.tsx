9a7d906e137ea4e82066433fbb624a83da5e5b20
Pande <popovski.pande@gmail.com>
2023-09-05 13:06:13 +0200

    style Hero for mobile/desktop

--- src/assets/components/Hero.tsx
+++ src/assets/components/Hero.tsx
@@ -1,10 +1,10 @@
 function Hero() {
   return(
     <section>
-      <div className="relative flex flex-col items-center p-10 pb-20 gap-8 bg-gray-950 bg-opacity-60">
+      <div className="relative flex flex-col items-center p-10 pb-20 gap-8 bg-gray-950 bg-opacity-60 md:mx-auto lg:py-40 lg:gap-16">
         {/* Content */}
-        <h1 className="text-gray-100 my-8 text-5xl font-bold text-center">The Most Competent IT Staff At Your Disposal</h1>
-        <p className=" text-gray-100  text-xl text-center">
+        <h1 className="text-gray-100 my-8 text-5xl font-bold text-center md:max-w-2xl lg:text-6xl">The Most Competent IT Staff At Your Disposal</h1>
+        <p className=" text-gray-100  text-xl text-center md:max-w-2xl lg:text-2xl">
           We at ITBuddy like to get down to the nitty-gritty of anything IT related. Our team is filled with the most experienced and professional Developers, Designers, SEO Experts and more.
         </p>
         <a href="" 
